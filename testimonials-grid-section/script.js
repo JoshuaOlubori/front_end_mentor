a2fce6fdd75934ca6cf867c63c0fe440546bdb4d
Edun Joshua Olubori <69902377+JoshuaOlubori@users.noreply.github.com>
2024-08-16 13:16:00 +0100

    Add files via upload

--- testimonials-grid-section/script.js
+++ testimonials-grid-section/script.js
@@ -483,3 +483,11 @@ export class Cluster extends HTMLElement {
 if ('customElements' in window) {
   customElements.define('cluster-l', Cluster);
 }
+
+/**
+ * @module grid-l
+ * @description
+ * A custom element for creating a responsive grid using the CSS Grid module
+ * @property {string} min=250px A CSS length value representing x in `minmax(min(x, 100%), 1fr)`
+ * @property {string} space=var(--s1) The space between grid cells
+ */
